1a8880ad3a692983596d021d19869ae3a0483fb7
hgratz <70618511+hgratz@users.noreply.github.com>
2020-09-07 12:08:12 -0500

    Commitnew

---
+++ gittst.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+int main (int argc, char *argv[]) 
+{ 
+    
+    double input[2][10]; //you can use a vector of vectors, or a 2d C-style array if needed
+ 
+
+    for (int i =0; i<2;i++)
+    {
+        for(int j=0;j<10;j++)
+        {
+            cin>>input[i][j];
+        }
+        
+    }
+    for (int i =0; i<2;i++)
+    {
+        for(int j=0;j<10;j++)
+        {
+            cout<<input[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+     
+    return 1;
+
+} 
